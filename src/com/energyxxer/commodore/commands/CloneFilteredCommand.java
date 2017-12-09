7f7ac2c19bdafe0c8af03103a6ee2fe528236e16
Energyxxer <energyxxer@gmail.com>
2017-12-09 19:28:56 -0400

    Made clone filtered use block

--- src/com/energyxxer/commodore/commands/CloneFilteredCommand.java
+++ src/com/energyxxer/commodore/commands/CloneFilteredCommand.java
@@ -1,18 +1,18 @@
 package com.energyxxer.commodore.commands;
 
+import com.energyxxer.commodore.block.Block;
 import com.energyxxer.commodore.coordinates.CoordinateSet;
-import com.energyxxer.commodore.types.BlockType;
 
 public class CloneFilteredCommand extends CloneCommand {
 
-    private BlockType block;
+    private Block block;
 
-    public CloneFilteredCommand(CoordinateSet source1, CoordinateSet source2, CoordinateSet destination, BlockType block) {
+    public CloneFilteredCommand(CoordinateSet source1, CoordinateSet source2, CoordinateSet destination, Block block) {
         super(source1, source2, destination);
         this.block = block;
     }
 
-    public CloneFilteredCommand(CoordinateSet source1, CoordinateSet source2, CoordinateSet destination, BlockType block, SourceMode sourceMode) {
+    public CloneFilteredCommand(CoordinateSet source1, CoordinateSet source2, CoordinateSet destination, Block block, SourceMode sourceMode) {
         super(source1, source2, destination, sourceMode);
         this.block = block;
     }
---
+++ src/com/energyxxer/commodore/commands/FillCommand.java
@@ -0,0 +1,4 @@
+package com.energyxxer.commodore.commands;
+
+public class FillCommand {
+}
