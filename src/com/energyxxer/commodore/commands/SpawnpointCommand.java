661949882cda63d4c2e804d73cef3244f842d343
Energyxxer <energyxxer@gmail.com>
2017-12-09 20:31:05 -0400

    Added SpawnpointCommand

---
+++ src/com/energyxxer/commodore/commands/SpawnpointCommand.java
@@ -0,0 +1,22 @@
+package com.energyxxer.commodore.commands;
+
+import com.energyxxer.commodore.Command;
+import com.energyxxer.commodore.coordinates.Coordinate;
+import com.energyxxer.commodore.coordinates.CoordinateSet;
+import com.energyxxer.commodore.entity.Entity;
+
+public class SpawnpointCommand implements Command {
+
+    private Entity player;
+    private CoordinateSet pos;
+
+    public SpawnpointCommand(Entity player, CoordinateSet pos) {
+        this.player = player;
+        this.pos = pos;
+    }
+
+    @Override
+    public String getRawCommand(Entity sender) {
+        return "spawnpoint " + player.getSelectorAs(sender) + " " + pos.getAs(Coordinate.DisplayMode.BLOCK_POS);
+    }
+}
