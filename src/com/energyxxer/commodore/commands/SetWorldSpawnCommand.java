1a02c57234d83a46c4927839d27802ee4c0681bd
Energyxxer <energyxxer@gmail.com>
2017-12-09 20:27:27 -0400

    Added SetWorldSpawnCommand

---
+++ src/com/energyxxer/commodore/commands/SetWorldSpawnCommand.java
@@ -0,0 +1,20 @@
+package com.energyxxer.commodore.commands;
+
+import com.energyxxer.commodore.Command;
+import com.energyxxer.commodore.coordinates.Coordinate;
+import com.energyxxer.commodore.coordinates.CoordinateSet;
+import com.energyxxer.commodore.entity.Entity;
+
+public class SetWorldSpawnCommand implements Command {
+
+    private CoordinateSet pos;
+
+    public SetWorldSpawnCommand(CoordinateSet pos) {
+        this.pos = pos;
+    }
+
+    @Override
+    public String getRawCommand(Entity sender) {
+        return "setworldspawn " + pos.getAs(Coordinate.DisplayMode.BLOCK_POS);
+    }
+}
