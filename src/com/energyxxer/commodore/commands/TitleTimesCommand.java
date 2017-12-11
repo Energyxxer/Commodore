991948ce15bb4aafa194824a0414ba20db17c439
Energyxxer <energyxxer@gmail.com>
2017-12-11 00:04:24 -0400

    Added TitleCommand and TitleTimesCommand

---
+++ src/com/energyxxer/commodore/commands/TitleTimesCommand.java
@@ -0,0 +1,23 @@
+package com.energyxxer.commodore.commands;
+
+import com.energyxxer.commodore.Command;
+import com.energyxxer.commodore.entity.Entity;
+
+public class TitleTimesCommand implements Command {
+    private Entity player;
+    private int fadeIn;
+    private int stay;
+    private int fadeOut;
+
+    public TitleTimesCommand(Entity player, int fadeIn, int stay, int fadeOut) {
+        this.player = player;
+        this.fadeIn = fadeIn;
+        this.stay = stay;
+        this.fadeOut = fadeOut;
+    }
+
+    @Override
+    public String getRawCommand(Entity sender) {
+        return "title " + player.getSelectorAs(sender) + " times " + fadeIn + " " + stay + " " + fadeOut;
+    }
+}
