6b23a05dbaf467f1f2f06f37f91ee67f30d8dc06
Energyxxer <energyxxer@gmail.com>
2018-02-10 19:05:28 -0400

    Added scoreboard accesses to trigger command

--- src/com/energyxxer/commodore/commands/trigger/TriggerCommand.java
+++ src/com/energyxxer/commodore/commands/trigger/TriggerCommand.java
@@ -1,19 +1,35 @@
 package com.energyxxer.commodore.commands.trigger;
 
 import com.energyxxer.commodore.commands.Command;
+import com.energyxxer.commodore.functions.Function;
 import com.energyxxer.commodore.inspection.CommandResolution;
 import com.energyxxer.commodore.inspection.ExecutionContext;
+import com.energyxxer.commodore.score.LocalScore;
 import com.energyxxer.commodore.score.Objective;
+import com.energyxxer.commodore.score.access.ScoreboardAccess;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class TriggerCommand implements Command {
 
     public enum Action {
-        SET, ADD
+        SET(false), ADD(true);
+
+        boolean readsScore;
+
+        Action(boolean readsScore) {
+            this.readsScore = readsScore;
+        }
     }
 
     private Objective objective;
     private Action action;
     private int amount;
+
+    private ExecutionContext execContext;
+    private ArrayList<ScoreboardAccess> accesses;
 
     public TriggerCommand(Objective objective) {
         this(objective, Action.ADD, 1);
@@ -25,6 +41,33 @@ public class TriggerCommand implements Command {
         this.objective = objective;
         this.action = action;
         this.amount = amount;
+    }
+
+    private void createScoreboardAccesses() {
+        if(accesses != null) return;
+        accesses = new ArrayList<>();
+
+        LocalScore localScore = new LocalScore(objective, execContext.getFinalSender());
+
+        if(action.readsScore) {
+            ScoreboardAccess access2 = new ScoreboardAccess(localScore.getMacroScores(), ScoreboardAccess.AccessType.WRITE);
+            ScoreboardAccess access1 = new ScoreboardAccess(localScore.getMacroScores(), ScoreboardAccess.AccessType.READ, access2);
+            accesses.add(access1);
+            accesses.add(access2);
+        } else {
+            accesses.add(new ScoreboardAccess(localScore.getMacroScores(), ScoreboardAccess.AccessType.WRITE));
+        }
+    }
+
+    @Override
+    public @NotNull Collection<ScoreboardAccess> getScoreboardAccesses() {
+        if(accesses == null) createScoreboardAccesses();
+        return accesses;
+    }
+
+    @Override
+    public void onAppend(Function function, ExecutionContext execContext) {
+        this.execContext = execContext;
     }
 
     @Override
