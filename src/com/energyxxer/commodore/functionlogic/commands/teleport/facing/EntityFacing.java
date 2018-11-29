c4077879416ee76421eee7df9a43e1ac9b78c7ad
Energyxxer <energyxxer@gmail.com>
2018-11-28 22:28:42 -0400

    Add teleport facing anchor support

--- src/com/energyxxer/commodore/functionlogic/commands/teleport/facing/EntityFacing.java
+++ src/com/energyxxer/commodore/functionlogic/commands/teleport/facing/EntityFacing.java
@@ -1,5 +1,6 @@
 package com.energyxxer.commodore.functionlogic.commands.teleport.facing;
 
+import com.energyxxer.commodore.functionlogic.commands.execute.EntityAnchor;
 import com.energyxxer.commodore.functionlogic.entity.Entity;
 import com.energyxxer.commodore.functionlogic.inspection.CommandEmbeddable;
 import com.energyxxer.commodore.functionlogic.score.access.ScoreboardAccess;
@@ -9,14 +10,20 @@ import java.util.Collections;
 
 public class EntityFacing implements TeleportFacing {
     private final Entity entity;
+    private final EntityAnchor anchor;
 
     public EntityFacing(Entity entity) {
+        this(entity, EntityAnchor.FEET);
+    }
+
+    public EntityFacing(Entity entity, EntityAnchor anchor) {
         this.entity = entity;
+        this.anchor = anchor;
     }
 
     @Override
     public String getRaw() {
-        return "facing entity \be#";
+        return "facing entity \be# " + anchor.toString().toLowerCase();
     }
 
     @Override
