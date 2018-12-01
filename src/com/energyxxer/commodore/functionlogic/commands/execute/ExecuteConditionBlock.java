81dda033123844e0303cd316c1286a9d2bc31152
Energyxxer <energyxxer@gmail.com>
2018-12-01 17:53:30 -0400

    Make if/unless block use Block class

--- src/com/energyxxer/commodore/functionlogic/commands/execute/ExecuteConditionBlock.java
+++ src/com/energyxxer/commodore/functionlogic/commands/execute/ExecuteConditionBlock.java
@@ -1,27 +1,29 @@
 package com.energyxxer.commodore.functionlogic.commands.execute;
 
+import com.energyxxer.commodore.block.Block;
 import com.energyxxer.commodore.functionlogic.coordinates.Coordinate;
 import com.energyxxer.commodore.functionlogic.coordinates.CoordinateSet;
 import com.energyxxer.commodore.functionlogic.inspection.ExecutionContext;
 import com.energyxxer.commodore.functionlogic.inspection.ExecutionVariableMap;
 import com.energyxxer.commodore.types.Type;
 
-import static com.energyxxer.commodore.types.TypeAssert.assertBlock;
-
 public class ExecuteConditionBlock extends ExecuteCondition {
     private final CoordinateSet pos;
-    private final Type blockType;
+    private final Block block;
 
     public ExecuteConditionBlock(ConditionType flowController, CoordinateSet pos, Type blockType) {
+        this(flowController, pos, new Block(blockType));
+    }
+
+    public ExecuteConditionBlock(ConditionType flowController, CoordinateSet pos, Block block) {
         super(flowController);
-        assertBlock(blockType);
         this.pos = pos;
-        this.blockType = blockType;
+        this.block = block;
     }
 
     @Override
     public SubCommandResult getSubCommand(ExecutionContext execContext) {
-        return new SubCommandResult(execContext, this.getStarter() + "block " + pos.getAs(Coordinate.DisplayMode.BLOCK_POS) + " " + blockType);
+        return new SubCommandResult(execContext, this.getStarter() + "block " + pos.getAs(Coordinate.DisplayMode.BLOCK_POS) + " " + block);
     }
 
     @Override
