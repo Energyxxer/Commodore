6dd53bfc50d3e969cb2aff7d303c99c7b8aff467
Energyxxer <energyxxer@gmail.com>
2018-12-02 19:19:48 -0400

    Fix maxEntityCramming requiring a boolean

--- src/com/energyxxer/commodore/functionlogic/commands/gamerule/GameruleQueryCommand.java
+++ src/com/energyxxer/commodore/functionlogic/commands/gamerule/GameruleQueryCommand.java
@@ -2,14 +2,18 @@ package com.energyxxer.commodore.functionlogic.commands.gamerule;
 
 import com.energyxxer.commodore.functionlogic.inspection.CommandResolution;
 import com.energyxxer.commodore.functionlogic.inspection.ExecutionContext;
+import com.energyxxer.commodore.types.Type;
 import org.jetbrains.annotations.NotNull;
+
+import static com.energyxxer.commodore.types.TypeAssert.assertGamerule;
 
 public class GameruleQueryCommand extends GameruleCommand {
 
-    private final String gamerule;
+    private final Type gamerule;
 
-    public GameruleQueryCommand(String gamerule) {
+    public GameruleQueryCommand(Type gamerule) {
         this.gamerule = gamerule;
+        assertGamerule(gamerule);
     }
 
     @Override @NotNull
--- src/com/energyxxer/commodore/functionlogic/commands/gamerule/GameruleSetCommand.java
+++ src/com/energyxxer/commodore/functionlogic/commands/gamerule/GameruleSetCommand.java
@@ -2,16 +2,21 @@ package com.energyxxer.commodore.functionlogic.commands.gamerule;
 
 import com.energyxxer.commodore.functionlogic.inspection.CommandResolution;
 import com.energyxxer.commodore.functionlogic.inspection.ExecutionContext;
+import com.energyxxer.commodore.types.Type;
 import org.jetbrains.annotations.NotNull;
+
+import static com.energyxxer.commodore.types.TypeAssert.assertGamerule;
 
 public class GameruleSetCommand extends GameruleCommand {
 
-    private final String gamerule;
+    private final Type gamerule;
     private final String value;
 
-    public GameruleSetCommand(String gamerule, Object value) {
+    public GameruleSetCommand(Type gamerule, Object value) {
         this.gamerule = gamerule;
         this.value = String.valueOf(value);
+
+        assertGamerule(gamerule);
     }
 
     @Override @NotNull
