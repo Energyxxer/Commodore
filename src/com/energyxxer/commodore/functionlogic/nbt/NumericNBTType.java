78aee289da8fa892a633f6fa9a8d5b1b860bb69b
Energyxxer <energyxxer@gmail.com>
2019-03-09 00:19:50 -0400

    Add getTypeForSuffix method in NumericNBTType enum

--- src/com/energyxxer/commodore/functionlogic/nbt/NumericNBTType.java
+++ src/com/energyxxer/commodore/functionlogic/nbt/NumericNBTType.java
@@ -1,5 +1,7 @@
 package com.energyxxer.commodore.functionlogic.nbt;
 
+import org.jetbrains.annotations.NotNull;
+
 public enum NumericNBTType {
     BYTE("b"), DOUBLE("d"), FLOAT("f"), INT(""), LONG("L"), SHORT("s");
 
@@ -11,5 +13,12 @@ public enum NumericNBTType {
 
     public String getSuffix() {
         return suffix;
+    }
+
+    public static NumericNBTType getTypeForSuffix(@NotNull String suffix) {
+        for(NumericNBTType type : values()) {
+            if(type.suffix.toLowerCase().equals(suffix.toLowerCase())) return type;
+        }
+        return null;
     }
 }
